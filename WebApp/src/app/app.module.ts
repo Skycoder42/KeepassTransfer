807fe43c388da3a5945f9ff3834f8a1d771f68f0
Skycoder42 <Skycoder42@users.noreply.github.com>
2016-11-28 22:34:09 +0100

    added help component
    static only --> make dynamic as needed

--- WebApp/src/app/app.module.ts
+++ WebApp/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HelpComponent } from './help/help.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelpComponent
   ],
   imports: [
     BrowserModule,
---
+++ WebApp/src/app/help/help.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-help',
+  templateUrl: './help.component.html',
+  styleUrls: ['./help.component.css']
+})
+export class HelpComponent {
+
+  constructor() { }
+
+}
