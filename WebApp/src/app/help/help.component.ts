8023283cab6706047e93268beb82de1bd1a1da51
Skycoder42 <Skycoder42@users.noreply.github.com>
2016-11-29 18:32:51 +0100

    added qr config with updating the form
    form works, but no validation!

--- WebApp/src/app/help/help.component.ts
+++ WebApp/src/app/help/help.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { QrConfig } from "../qr-config";
 
 @Component({
   selector: 'app-help',
@@ -6,7 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./help.component.css']
 })
 export class HelpComponent {
+  public keySizes = [512, 1024, 2048, 4096, 8192];
+  public errorLevels = [
+    {key:"Level L (Low)",value:1},
+    {key:"Level M (Medium)",value:0},
+    {key:"Level Q (Quartile)",value:3},
+    {key:"Level H (High)",value:2}
+  ];
 
-  constructor() { }
+  public currentConfig = new QrConfig();
 
+  onSubmit() {
+    console.log(this.currentConfig.qrSize);
+  }
 }
---
+++ WebApp/src/app/qr-config.ts
@@ -0,0 +1,5 @@
+export class QrConfig {
+  public keySize: number = 2048;
+  public errorLevel: number = 1;
+  public qrSize: number = 512;
+}
