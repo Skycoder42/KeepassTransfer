ec6d3aca1ef2bb9a98db1ab60d1e8ff6215395a5
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-01-07 14:16:40 +0100

    updated to 1.2.1 background process

--- TransferBackend/main.cpp
+++ TransferBackend/main.cpp
@@ -2,55 +2,24 @@
 
 #include <QCommandLineParser>
 #include <QCoreApplication>
-#include <QBackgroundProcess>
+#include <QtBackgroundProcess>
 #include <QLoggingCategory>
 
 int main(int argc, char *argv[])
 {
-	QBackgroundProcess::App a(argc, argv);
+	QtBackgroundProcess::App a(argc, argv);
 	QCoreApplication::setApplicationName(TARGET);
 	QCoreApplication::setApplicationVersion(VERSION);
 	QCoreApplication::setOrganizationName(COMPANY);
 	QCoreApplication::setOrganizationDomain("com.skycoder42");
-	QBackgroundProcess::App::activateTerminalDebugRedirect();
 
-	QCommandLineParser parser;
-	parser.setApplicationDescription(DISPLAY_NAME);
-	parser.addHelpOption();
-	parser.addVersionOption();
-	parser.addPositionalArgument("start|stop", "Starts or stops the application", "[start|stop]");
-	parser.addPositionalArgument("port", "The port to listen on");
-	parser.addOption({
-						 {"l", "log"},
-						 "Set the desired log <level>. Possible values are:\n"
-						 " - 0: log nothing\n"
-						 " - 1: critical errors only\n"
-						 " - 2: 1 plus warnings\n"
-						 " - 3: 2 plus information messages\n"
-						 " - 4: verbose - log everything",
-						 "level",
-						 "3"
-					 });
-	parser.process(a);
+	a.setParserSetupFunction([](QCommandLineParser &parser){
+		parser.setApplicationDescription(DISPLAY_NAME);
+		parser.addPositionalArgument("port", "The port to listen on.");
+	});
 
-	QString logStr;
-	switch (parser.value("l").toInt()) {
-	case 0:
-		logStr.prepend("\n*.critical=false");
-	case 1:
-		logStr.prepend("\n*.warning=false");
-	case 2:
-		logStr.prepend("\n*.info=false");
-	case 3:
-		logStr.prepend("*.debug=false");
-	default:
-		break;
-	}
-	if(!logStr.isNull())
-		QLoggingCategory::setFilterRules(logStr);
-
-	a.setStartupFunction([&parser](QStringList) {
-		QBackgroundProcess::App::activateMasterDebugRedirect();
+	a.setStartupFunction([](const QCommandLineParser &parser) {
+		qApp->setForwardMasterLog(true);
 		if(parser.positionalArguments().size() < 2)
 			return EXIT_FAILURE;
 		auto server = new AppServer(qApp);
