edc992a4066a90911f0cff2ed8933462492f287c
Skycoder42 <Skycoder42@users.noreply.github.com>
2018-11-28 19:49:41 +0100

    migrate browserstorage to use emscripten::val

--- webapp/browserstorage_wasm.cpp
+++ webapp/browserstorage_wasm.cpp
@@ -3,6 +3,8 @@
 #include <emscripten.h>
 #include <emscripten/html5.h>
 #include <emscripten/bind.h>
+#include <emscripten/val.h>
+using namespace emscripten;
 
 BrowserStorage::BrowserStorage(QObject *parent) :
 	QObject{parent}
@@ -10,70 +12,47 @@ BrowserStorage::BrowserStorage(QObject *parent) :
 
 int BrowserStorage::keyCount() const
 {
-	return EM_ASM_INT({
-		var store = window.localStorage;
-		console.log('before store');
-		console.log(store);
-		console.log('after store');
-		console.log(typeof store.length);
-		console.log(store.length);
-		console.log('after length');
-		return store.length;
-	});
+	auto store = val::global("localStorage");
+	Q_ASSERT(!store.isNull());
+	return store["length"].as<int>();
 }
 
 QString BrowserStorage::key(int index) const
 {
-	auto intPtr = EM_ASM_INT({
-		var key = window.localStorage.key($0);
-		var resPtr = 0;
-		if(key)
-			resPtr = allocate(intArrayFromString(key), 'i8', ALLOC_NORMAL);
-		return resPtr;
-	}, index);
-	return intPtr == 0 ?
-				QString{} :
-				QLatin1String{reinterpret_cast<char*>(intPtr)};
+	auto store = val::global("localStorage");
+	Q_ASSERT(!store.isNull());
+	const auto key = store.call<std::string>("key", index);
+	return QString::fromStdString(key);
 }
 
 QVariant BrowserStorage::value(const QString &key, const QVariant &defaultValue) const
 {
-	const auto keyData = key.toLocal8Bit();
-	auto intPtr = EM_ASM_INT({
-		var value = window.localStorage.getItem(Pointer_stringify($0));
-		var resPtr = 0;
-		if(value)
-			resPtr = allocate(intArrayFromString(value), 'i8', ALLOC_NORMAL);
-		return resPtr;
-	}, keyData.constData());
-	return intPtr == 0 ?
-				defaultValue :
-				QVariant{QLatin1String{reinterpret_cast<char*>(intPtr)}};
+	auto store = val::global("localStorage");
+	Q_ASSERT(!store.isNull());
+	const auto value = store.call<val>("getItem", key.toStdString());
+	if(value.isNull())
+		return defaultValue;
+	else
+		return QString::fromStdString(value.as<std::string>());
 }
 
 void BrowserStorage::setValue(const QString &key, const QVariant &value)
 {
-	const auto keyData = key.toLocal8Bit();
-	const auto valueData = value.toString().toLocal8Bit();
-	EM_ASM_ARGS({
-		var key = Pointer_stringify($0);
-		var value = Pointer_stringify($1);
-		window.localStorage.setItem(key, value);
-	}, keyData.constData(), valueData.constData());
+	auto store = val::global("localStorage");
+	Q_ASSERT(!store.isNull());
+	store.call<void>("setItem", key.toStdString(), value.toString().toStdString());
 }
 
 void BrowserStorage::remove(const QString &key)
 {
-	const auto keyData = key.toLocal8Bit();
-	EM_ASM_ARGS({
-		var key = Pointer_stringify($0);
-		window.localStorage.removeItem(key);
-	}, keyData.constData());
+	auto store = val::global("localStorage");
+	Q_ASSERT(!store.isNull());
+	store.call<void>("removeItem", key.toStdString());
 }
 
 void BrowserStorage::clear()
 {
-	EM_ASM({
-		window.localStorage.clear();
-	});
+	auto store = val::global("localStorage");
+	Q_ASSERT(!store.isNull());
+	store.call<void>("clear");
 }
